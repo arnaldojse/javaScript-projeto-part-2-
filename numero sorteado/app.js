560a1e41c2952e484be419f87a798f2e2a7541f3
arnaldojse <arnaldojoseboy@gmail.com>
2025-03-02 14:08:30 -0300

    codigo comentado

--- numero sorteado/app.js
+++ numero sorteado/app.js
@@ -1,13 +1,18 @@
-/*(document.querySelector('h1 ou qualquer outro nome da tag que vamos utilizar')) -> pegue no documento a query selecionada como h1 # 
- (titulo.innerHTML = 'escreva oque você quer colocar no titulo ')  -> cloque  dentro do html o titulo escrita ao lado pro meio de uma determinada variavel
+/*(document.querySelector('h1 ou qualquer outro nome da tag que vamos utilizar')) -> pegue no documento a query selecionada como h1;
+
+ (titulo.innerHTML = 'escreva oque você quer colocar no titulo ')  -> altera o conteúdo interno (innerHTML) do titulo (h1) escrito ao lado pro meio de uma determinada variavel;
+
+ function servae para criar uma função nesse casso a função verificarChute() que vai ser chamada pelo html todas as vezes que o botão for aperdado 
+ function vrrificarChute(){esta e a forma escrita da functioin
+  } 
 */
 
 let titulo = document.querySelector('h1'); 
-titulo.innerHTML = 'Numero Secreto';
+titulo.innerHTML = 'Hora do desfio';
 
-let paragrafo = DocumentTimeline.querySelector('p')
-paragrafo.innerHTML = 'selecione um numero de 1 até 10'
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'selecione um numero de 1 até 10';
 
 function verificarChute(){
-    
+    console.log('o botão foi clicado')
 }
