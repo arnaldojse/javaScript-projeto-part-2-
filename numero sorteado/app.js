e665e599ba5d8c84ed45d224da1b68fbc9e42410
arnaldojse <arnaldojoseboy@gmail.com>
2025-03-02 10:21:36 -0300

    a

---
+++ numero sorteado/app.js
@@ -0,0 +1,13 @@
+/*(document.querySelector('h1 ou qualquer outro nome da tag que vamos utilizar')) -> pegue no documento a query selecionada como h1 # 
+ (titulo.innerHTML = 'escreva oque você quer colocar no titulo ')  -> cloque  dentro do html o titulo escrita ao lado pro meio de uma determinada variavel
+*/
+
+let titulo = document.querySelector('h1'); 
+titulo.innerHTML = 'Numero Secreto';
+
+let paragrafo = DocumentTimeline.querySelector('p')
+paragrafo.innerHTML = 'selecione um numero de 1 até 10'
+
+function verificarChute(){
+    
+}
