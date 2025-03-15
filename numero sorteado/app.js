315ac20c1971402ec4f8411730cd9a3b009eb53a
arnaldojse <arnaldojoseboy@gmail.com>
2025-03-15 14:27:19 -0300

    quase no fim

--- numero sorteado/app.js
+++ numero sorteado/app.js
@@ -5,10 +5,34 @@
  function servae para criar uma função nesse casso a função verificarChute() que vai ser chamada pelo html todas as vezes que o botão for aperdado 
  function vrrificarChute(){esta e a forma escrita da functioin
   } 
+(.value) no final do document.querySelect(parametro).VALUE faz com que apenas o valor referente a query selecionada seja selecionado   
 
+uma forma diferente de fazer uma comparação (condição ? valor_se_verdadeiro : valor_se_falso;)
 _______________________________________________________________________________  
  /*function exibirAlert(){
-    alert('Eu amo js')       apenas um exercicio
+    alert('Eu amo js')       apenas exercicios
+==================================================
+    function olaMundoConsole (){
+  console.log('olaaaaa mundo');
+}
+olaMundoConsole();
+==================================================
+function nomeDOoUsuario(nome) {
+  console.log(`ola ${nome}!`)
+}
+nomeDOoUsuario('Alice')
+==================================================
+function dobro(num){
+  return num * 2
+}
+let resultado = dobro(2)
+console.log(resultado)
+==================================================
+function media(a,b,c){
+  return (a + b + c) / 3
+}
+let resultado = media(4, 7, 10)
+console.log(resultado)
 _______________________________________________________________________________    
 }
 function somandoDoisNumeros() {
@@ -26,10 +50,12 @@ function exibirNomeTela(tag, Texto){
   campo.innerHTML = Texto;
 }
 exibirNomeTela('h1', ' Hora do desfio ');
-exibirNomeTela('p', 'selecione um numero de 1 até 100');
+exibirNomeTela('p', 'selecione um numero de 1 até 10');
 
 function verificarChute(){
-    console.log(numeroSecreto);
+    let chute = document.querySelector('input').value
+    console.log(chute == numeroSecreto)
+    console.log(numeroSecreto)
 }
 
 function grrarNUmeroAleatorio() {
